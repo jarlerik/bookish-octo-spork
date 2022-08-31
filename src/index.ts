d34b51685f3e319f8759e6456ca4b47846bdc8dd
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2022-08-31 18:15:45 +0300

    Added sass

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import component from "./component";
-import index from "./index.module.css";
+import index from "./index.module.scss";
 document.body.className = index.main;
 document.body.appendChild(component("Hello Webpack live with ts! Woop woop!"));
