4ea3e2b894bef85adb17c43dd09430a2b91d2ed0
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2022-08-31 21:56:47 +0300

    Added CssMinimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("node:path");
 const { mode } = require("webpack-nano/argv");
 const { merge } = require("webpack-merge");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devServer = require("./webpack-config/devServer");
 const html = require("./webpack-config/html");
 const babel = require("./webpack-config/babel");
@@ -14,7 +16,17 @@ const commonConfig = (mode) =>
     html.page({ title: "Webpak" }),
   ]);
 
-const productionConfig = merge([]);
+const productionConfig = merge([
+  {
+    optimization: {
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        // `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
+  },
+]);
 
 const developmentConfig = merge([devServer.serve()]);
 
